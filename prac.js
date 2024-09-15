899cf7b0619c7aeb3c2e0e95974c81298fdd7535
Sushil Ingole <sushil.ingole@nihilent.com>
2024-09-15 21:06:39 +0530

    Practice files

--- prac.js
+++ prac.js
@@ -1,17 +1,34 @@
-let arr = [1, 2, [3, 4], [5, [6, 7], 8], 9];
-let sum = 0;
-function flatten(num) {
-    if (isNaN(num)) {
-        for (let i = 0; i < num.length; i++) {
-            flatten(num[i]);
+// function nextSmallerElement(arr, n) { // O(n^2)
+//     let ans = [];
+//     for(let i=0; i<n; i++) {
+//         let curr = arr[i];
+//         let nextMinEle = curr;
+//         for(let j=i+1; j<n; j++) {
+//             if(arr[j] < nextMinEle) {
+//                 nextMinEle = arr[j];
+//                 break;
+//             }
+//         }
+//         ans.push(nextMinEle === curr ? -1 : nextMinEle);
+//     }
+//     console.log(ans);
+// }
+
+function nextSmallerElement(arr, n) { // O(n)
+    let ans = [], stack = [-1];
+    for(let i=n-1; i>=0; i--) {
+        let curr = arr[i];
+        let stTop = stack[stack.length-1];
+        while(stTop !== -1 && arr[stTop] >= curr) {
+            stack.pop();
+            stTop = stack[stack.length-1];
         }
-    } else {
-        sum += num;
+        ans[i] = stTop === -1 ? stTop : arr[stTop];
+        stack.push(i);
     }
+    console.log(ans);
 }
 
-for (let i = 0; i < arr.length; i++) {
-    flatten(arr[i]);
-}
-
-console.log("sum: ", sum);
+// let arr = [2,1,4,3]; // [ 1, -1, 3, -1 ]
+let arr = [4, 8, 5, 2, 25]; // [ 2, 5, 2, -1, -1 ]
+nextSmallerElement(arr, arr.length);
