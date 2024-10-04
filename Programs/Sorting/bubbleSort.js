851008ff3e5d7a692abda3abf4b52f7c954a8f19
sush1999ingole6925 <sush1999ingole6925@gmail.com>
2024-10-04 10:41:16 +0530

    Bubble sort algorithm

--- Programs/Sorting/bubbleSort.js
+++ Programs/Sorting/bubbleSort.js
@@ -3,12 +3,12 @@ const numbers = [19, 12, 10, 11, 4, 111, 6, 112, 1, 7];
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let swapped = false;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 swapped = true;
-                const temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+                const temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
             }
         }
         if (!swapped) break;
